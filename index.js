c4ed06ac9d22b8ac21ec544fecc9d90bca4f5bf6
Dariga Iskakova <darigaiskakova@icloud.com>
2022-04-30 17:38:35 -0400

    laid the foundation of inquirer in my code

---
+++ index.js
@@ -0,0 +1,33 @@
+// TODO: Include packages needed for this application
+const inquirer= require("inquirer")
+const generate = require("./utils/generateMarkdown");
+const fs = require("fs");
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of this project?"
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if(err) throw err;
+        console.log("generated new readme")
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => {
+        console.log(answers)
+        writeToFile("README.md", generate(answers))
+    })
+}
+
+// Function call to initialize app
+init();
